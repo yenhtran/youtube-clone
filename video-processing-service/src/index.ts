9314dc1f46676bae315a48956912cdcbbac32b4a
Yen Tran <yentran@Yens-MBP.hawaiiantel.net>
2023-11-29 15:10:31 -1000

    process videos locally

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,35 @@
 import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.post('/process-video', (req, res) => {
+  // Get path of the input video file/from the request body
+  console.log('req.body: ', req.body);
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  // Check if the input file path is defined
+  if (!inputFilePath || !outputFilePath) {
+    res.status(400).send('Bad Request: Missing file path.');
+  }
+
+  // Create the ffmpeg command
+  ffmpeg(inputFilePath)
+    .outputOptions('-vf', 'scale=-1:360') // 360p
+    .on('end', () => {
+      console.log('Processing finished successfully');
+      res.status(200).send('Processing finished successfully.');
+    })
+    .on('error', (err) => {
+      console.log(`An error occured: ${err.message}`);
+      res.status(500).send(`Internal Server Error: ${err.message}`);
+    })
+    .save(outputFilePath);
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Video processing service listening at http://localhost:${port}`);
 });
